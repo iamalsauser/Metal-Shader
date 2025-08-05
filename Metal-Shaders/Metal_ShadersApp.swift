550833455ef004e2128bd92e314e562ea6420b5b
iamalsauser <parthsinhv4@gmail.com>
2025-08-05 12:45:52 +0530

    Update Metal_ShadersApp: add AVFoundation import for camera permissions

--- Metal-Shaders/Metal_ShadersApp.swift
+++ Metal-Shaders/Metal_ShadersApp.swift
@@ -6,9 +6,19 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 @main
 struct Metal_ShadersApp: App {
+    init() {
+        // Request camera permission on app launch
+        AVCaptureDevice.requestAccess(for: .video) { granted in
+            if !granted {
+                print("Camera access denied")
+            }
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
